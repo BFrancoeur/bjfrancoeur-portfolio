3ffca95f0d869bfdc13df73931838de2e5d5378a
Brian Francoeur <francoeurbj@gmail.com>
2022-09-29 16:08:57 -0400

    create about me section

--- src/components/AboutMe/AboutMe.component.js
+++ src/components/AboutMe/AboutMe.component.js
@@ -3,21 +3,82 @@ import {
   AboutMeLeft,
   AboutMeTitle,
   AboutMeRole,
+  AboutMeOldRole,
+  AboutMeNewRole,
   AboutMeDivider,
+  AboutMeRight,
+  AboutMeParagraph,
+  AboutMeParaLead,
+  AboutMeEm,
 } from "./AboutMe.styles";
+
+import SocialLinkSection from "../SocialLinks/SocialLinks.component";
 
 const AboutMeSection = () => {
   return (
     <AboutMe>
       <AboutMeLeft>
         <AboutMeTitle>About Me</AboutMeTitle>
-        <AboutMeRole>I'm a Web Developer</AboutMeRole>
+        <AboutMeRole>
+          <AboutMeOldRole>Failed Entrepreneur</AboutMeOldRole> <br />
+          Turned <AboutMeNewRole>Web Developer</AboutMeNewRole>
+        </AboutMeRole>
         <AboutMeDivider />
       </AboutMeLeft>
 
-      <div className="col col-right">
-        <h2>Right-Hand Column</h2>
-      </div>
+      <AboutMeRight>
+        <AboutMeParagraph>
+          <AboutMeParaLead>
+            Failure is a lens that provides clarity.{" "}
+          </AboutMeParaLead>
+          I thought I was someone who could do a hundred things and wear a dozen
+          hats every day, but it turns out that I can't. It's just not me. I
+          love to focus on one thing and get it right. I'm a craftsman who loves
+          to solve problems with code. I'm a pit bull who won't let go of a
+          problem until I've solved it. If I were to give my professional
+          persona a name, it would be Pittman. Weird, I know.
+        </AboutMeParagraph>
+
+        <AboutMeParagraph>
+          <AboutMeParaLead>
+            Web Development is so much more than merely writing code.{" "}
+          </AboutMeParaLead>
+          It's kind of like the composition of music. Modules are movements and
+          the front end is the performance. The many different bits of software
+          and scripts that run in the background are the musicians in the
+          orchestra. It's no coincidence that Composer is a popular dependency
+          manager for PHP, the scripting language that runs nearly all of the
+          websites in the world.
+        </AboutMeParagraph>
+        <AboutMeParagraph>
+          <AboutMeParaLead>
+            To create great work, you need to start with a great design system.{" "}
+          </AboutMeParaLead>
+          Sadly, this is almost never the case with web development. Too often,
+          websites and applications are created ad-hoc. After working on
+          hundreds of these kinds of websites (many of them use WordPress), I
+          decided to create a design and development system of my own using
+          React. My inspiration for this is from Brad Frost and his Atomic
+          Design philosophy. Finally, something that makes sense! You may think,{" "}
+          <AboutMeEm>Wait -- aren't design systems for designers? </AboutMeEm>{" "}
+          Well, yes they are.While building a design system in Figma, I had an
+          epiphany -- this is exactly what we need in web development! We need a
+          development system that parallels the design system. In fact, it
+          <AboutMeParaLead> incorporates</AboutMeParaLead> the design system
+          directly into the building of websites and applications. ReactJS is a
+          perfect fit for this approach. It's modular and readily lends itself
+          to this exact approach. This inspired me to begin work on a React
+          library of components based on Atomic Design and React's Styled
+          Components. This project is in its infancy, and I'm working out how to
+          define each component and, ultimately, how to implement a design
+          system using CSS/SASS to make the fundamental design changes like
+          color scheme and typography a matter of updating a handful of CSS
+          variables.
+        </AboutMeParagraph>
+
+        <p>Follow Me</p>
+        <SocialLinkSection />
+      </AboutMeRight>
     </AboutMe>
   );
 };
--- src/components/AboutMe/AboutMe.styles.jsx
+++ src/components/AboutMe/AboutMe.styles.jsx
@@ -7,6 +7,7 @@ export const AboutMe = styled.section`
     width: 100%;
     height: auto; 
     padding: 6.5rem 0;
+    color: 3fff4e5;
     background-color: #010016;
 `;
 
@@ -18,23 +19,49 @@ export const AboutMeLeft = styled.div`
 export const AboutMeTitle = styled.h2`
     font-size: 1.25rem;
     font-weight: 700;
-    color: #fff;
+    color: #fff4e5;
 `;
 
+
+
 export const AboutMeRole = styled.h3`
-    color: #fff;
+    color: #fff4e5;
     font-size: 3rem;
     font-weight: 300;
     line-height: 2em;
     margin-bottom: 2rem;
 `;
 
+export const AboutMeOldRole = styled.span`
+    color: #E02A00;
+`;
+
+export const AboutMeNewRole = styled.span`
+    color: #61B0E0;
+    font-weight: 500;
+`;
+
 export const AboutMeDivider = styled.hr`
     width: 6rem;
-    color: #fff;
+    color: #fff4e5;
 `;
 
 export const AboutMeRight = styled.div`
     flex: 1 0 50%;
     padding-right: 10%;
+`;
+
+export const AboutMeParagraph = styled.p`
+    color: #fff4e5;
+    font-size: 1.25rem;
+    line-height: 1.6em;
+    margin-bottom: 1.875rem;
+`;
+
+export const AboutMeEm = styled.em`
+    color: #61B0E0;
+`;
+
+export const AboutMeParaLead = styled.b`
+    color: #FAA500;
 `;
